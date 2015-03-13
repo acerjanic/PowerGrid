64fb315847a540602fa3f78843e106682ab91143
Alex Cerjanic <acerja2@illinois.edu>
2015-03-12 21:05:25 -0500

    Initial Commit

---
+++ PowerGrid/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PowerGrid
+//
+//  Created by Alex Cerjanic on 3/12/15.
+//  Copyright (c) 2015 MRFIL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
