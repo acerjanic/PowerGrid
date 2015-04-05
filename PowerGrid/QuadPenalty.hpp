19d56c433f796ba87af306c0c14cbebe0bc18351
Alex Cerjanic <acerja2@illinois.edu>
2015-04-05 15:44:36 -0500

    Pushing Quad Penalty in progress.

--- PowerGrid/QuadPenalty.hpp
+++ PowerGrid/QuadPenalty.hpp
@@ -9,5 +9,40 @@
 #ifndef PowerGrid_QuadPenalty_hpp
 #define PowerGrid_QuadPenalty_hpp
 
+using namespace arma;
+
+template <typename T1>
+class QuadPenalty
+{
+public:
+    QuadPenalty();
+    
+    //Class members
+    uword Nx;
+    uword Ny;
+    double DeltaX;
+    double DeltaY;
+    double Beta;
+    Col<uint_8> ReconMask;
+    
+    //Custom Class Constructor
+    QuadPenalty(uword Nx,uword Ny,double Beta,Col<uint_8> ReconMask)
+    {
+        //Set Class Memebers
+        this->Nx = Nx;
+        this->Ny = Ny;
+        this->DeltaX = 1.0/(double)Nx;
+        this->DeltaX = 1.0/(double)Ny;
+        this->ReconMask = ReconMask;
+        
+        //Create Sparse Differencing Matricies
+        sp_mat D = speye<sp_mat>(Nx,Ny);
+        
+        
+    }
+    
+    //Class Methods
+    
+};
 
 #endif
