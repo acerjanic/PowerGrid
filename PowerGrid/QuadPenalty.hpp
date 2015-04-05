26afa35960f9ea7d8d5fb234d487d07bb504f6f4
Alex Cerjanic <acerja2@illinois.edu>
2015-04-05 00:33:12 -0500

    Pushing a QuadPenalty file in progress.

---
+++ PowerGrid/QuadPenalty.hpp
@@ -0,0 +1,13 @@
+//
+//  QuadPenalty.hpp
+//  PowerGrid
+//
+//  Created by Alex Cerjanic on 4/4/15.
+//  Copyright (c) 2015 MRFIL. All rights reserved.
+//
+
+#ifndef PowerGrid_QuadPenalty_hpp
+#define PowerGrid_QuadPenalty_hpp
+
+
+#endif
