01f1954ab3f6962599060d4e5ba5951c0da2ae3d
Joe Holtrop <holtrop1@illinois.edu>
2015-04-05 19:26:06 -0500

    SENSE code update

--- PowerGrid/SENSE.hpp
+++ PowerGrid/SENSE.hpp
@@ -15,22 +15,56 @@ public:
     SENSE();
     
     //Class variables go here
+    uword n1 = 0; //Data size
+    uword n2 = 0; //Image size
+    uword nc = 0; //number of coils
+    Tobj *G_obj;
+    T1 SMap;
+
     
     //Class constructor
-    SENSE(Tobj G) {
-
+    SENSE(Tobj &G, T1 SENSEmap, uword a, uword b,uword c ) {
+      n1 = a;
+      n2 = b;
+      nc = c;
+      G_obj = &G;
+      SMap = SENSEmap;
     }
     
     //Overloaded operators go here
     
     //Forward transformation is *
     // d is the vector of data of type T1, note it is const, so we don't modify it directly rather return another vector of type T1
-    T1 operator*(const T1& d) {
-        //Refer to any class variables with this->
-        // ie this->variable1 to get to a class variable variable1
+    T1 operator*(const T1& d) const {
+
+      Mat<cx_double> outData = zeros<Mat<cx_double>>(this->n2,this->nc);
+
+      for (unsigned int ii=0; ii < this->nc; ii++) {
+        T1 data = d%(this->SMap.col(ii));
+
+        outData.col(ii) = (*this->G_obj)*(data);
+
+      }
+
+      //equivalent to returning col(output) in MATLAB with IRT
+      return vectorise(outData);
     }
     
-    T1 operator/(const T1& d) {
+    T1 operator/(const T1& d) const {
+
+      Mat<cx_double> inData = reshape(d,this->n2,this->nc);
+
+      Col<cx_double> outData = zeros<Col<cx_double>>(this->n1);
+
+      for (unsigned int ii=0; ii < this->nc; ii++) {
+        T1 data = inData.col(ii);
+        outData = outData + this->SMap.col(ii)%((*this->G_obj)/data);
+
+      }
+
+      //equivalent to returning col(output) in MATLAB with IRT
+      return vectorise(outData);
+
         
     }
 };
--- PowerGrid/main.cpp
+++ PowerGrid/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
     
     //Mat<cx_double> testComplex;
     //Load our read double matrix object. (You need to match type to avoide mangling the data.)
-    loadmat("/Users/alexcerjanic/Developer/PowerGrid/Resources/test.mat","test",&test);
+    loadmat("C:\\Ceemple\\user\\PowerGrid\\Resources\\test.mat","test",&test);
     
     //loadmat("/Users/alexcerjanic/Developer/PowerGrid/Resources/testForwardTest.mat","testForward",&testComplex);
     //Create our Gfft object for type complex double and N1=64, N2=64
@@ -37,18 +37,39 @@ int main(int argc, char** argv)
     //If we just passed the variable rather than the variable wrapped in a function, we wouldn't need the eval.
     //savemat("/Users/alexcerjanic/Developer/PowerGrid/Resources/testForwardOut.mat","testForwardOut",test);
 
-    savemat("/Users/alexcerjanic/Developer/PowerGrid/Resources/out.mat","fftShiftOut", fftshift(test).eval());
+    savemat("C:\\Ceemple\\user\\PowerGrid\\Resources\\out.mat","fftShiftOut", fftshift(test).eval());
     
     //Writing the transforms to disk for evaluation in MATLAB. Note that we can't read or write complex natively (yet), so lets
     //seperate real and imaginary first. Also, we can't put multiple variables in a single file yet. These are TODOs.
-    savemat("/Users/alexcerjanic/Developer/PowerGrid/Resources/testForwardReal.mat","testForwardReal", real(testForward).eval());
-    savemat("/Users/alexcerjanic/Developer/PowerGrid/Resources/testForwardImag.mat","testForwardImag", imag(testForward).eval());
-    savemat("/Users/alexcerjanic/Developer/PowerGrid/Resources/testForwardTest.mat","testForward", testForward);
-    savemat("/Users/alexcerjanic/Developer/PowerGrid/Resources/testAdjointTest.mat","testAdjoint", testAdjoint);
+    savemat("C:\\Ceemple\\user\\PowerGrid\\Resources\\testForwardReal.mat","testForwardReal", real(testForward).eval());
+    savemat("C:\\Ceemple\\user\\PowerGrid\\Resources\\testForwardImag.mat","testForwardImag", imag(testForward).eval());
+    savemat("C:\\Ceemple\\user\\PowerGrid\\Resources\\testForwardTest.mat","testForward", testForward);
+    savemat("C:\\Ceemple\\user\\PowerGrid\\Resources\\testAdjointTest.mat","testAdjoint", testAdjoint);
 
-    savemat("/Users/alexcerjanic/Developer/PowerGrid/Resources/testAdjointReal.mat","testAdjointReal", real(testAdjoint).eval());
-    savemat("/Users/alexcerjanic/Developer/PowerGrid/Resources/testAdjointImag.mat","testAdjointImag", imag(testAdjoint).eval());
+    savemat("C:\\Ceemple\\user\\PowerGrid\\Resources\\testAdjointReal.mat","testAdjointReal", real(testAdjoint).eval());
+    savemat("C:\\Ceemple\\user\\PowerGrid\\Resources\\testAdjointImag.mat","testAdjointImag", imag(testAdjoint).eval());
     
+    //Test SENSE
+
+    //Create an empty unsized matrix of type real double
+    Mat<double> SMap;
+
+    //Load our read double matrix object. (You need to match type to avoide mangling the data.)
+    loadmat("C:\\Ceemple\\user\\PowerGrid\\Resources\\SMap.mat","SMap",&SMap);
+
+    //create our SENSE object, coils =2
+    SENSE<Col<cx_double>, Gfft<Col<cx_double>>> S(G,vectorise(conv_to<Mat<cx_double>>::from(SMap)),4096*2,4096,2);
+
+    //Perform the forward transformation and store the result in a colum vector of type complex double
+     //Note the conv_to<type to convert to>::from(data) command to convert our real double phantom to type complex double
+     Col<cx_double> testForward_SENSE = S*vectorise(conv_to<Mat<cx_double>>::from(test));
+
+     //Now perform the adjoint transform
+     Col<cx_double> testAdjoint_SENSE = S/testForward_SENSE;
+
+     savemat("C:\\Ceemple\\user\\PowerGrid\\Resources\\testForwardTest_SENSE.mat","testForward_SENSE", testForward_SENSE);
+     savemat("C:\\Ceemple\\user\\PowerGrid\\Resources\\testAdjointTest_SENSE.mat","testAdjoint_SENSE", testAdjoint_SENSE);
+
     
     return 0;
 }
