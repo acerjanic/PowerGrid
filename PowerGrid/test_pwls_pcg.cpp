5c4d3e788ba5144a77c06321fcd4b26180785b28
Giang-Chau Ngo <gngo@illinois.edu>
2015-04-06 22:18:13 -0500

    Update the test case. Is the definition of W?

--- PowerGrid/test_pwls_pcg.cpp
+++ PowerGrid/test_pwls_pcg.cpp
@@ -5,24 +5,31 @@ using namespace arma;
 // Define code for testing
 Mat<cx_double> test_pwls_pcg()
 {
-   // Simulate the k-space data
-   Mat<cx_double> x = ones<Mat<cx_double>>(64,64);
 
-   Gfft<Col<cx_double>> G(64,64);
+   // Load real data
+   Mat<double> test;
 
-   Col<cx_double> y;
-   y = G *vectorise(x);
+   //Load our read double matrix object. (You need to match type to avoide mangling the data.)
+   loadmat("C:\\Ceemple\\user\\PowerGrid\\Resources\\test.mat","test",&test);
+
+   // Formard operator
+   Gfft<cx_double> G(64,64);
+   Col<cx_double> TestForwars;
+   TestForward = G *vectorise(conv_to<Mat<cx_double>>::from(test));
 
    // Variables needed for the recon: Penalty object, num of iterations 
    umat ReconMask;
    ReconMask.ones(64,64);
-   QuadPenalty<Col<cx_double>>R(64,64,1,ReconMask);
-    uword niter = 2;
+
+   QuadPenalty<cx_double>R(64,64,1,ReconMask);
+
+   uword niter = 2;
    Col<cx_double> xinit = zeros<Col<cx_double>>(64*64); // initial estimate of x
-    Mat<cx_double> W;
-    W = eye<Mat<cx_double>>(4096,4096);
-    Col<cx_double> x_t;
-    x_t = pwls_pcg1<Col<cx_double>,Gfft<Col<cx_double>>,QuadPenalty<Col<cx_double>>>(xinit, G, W, y, R, niter);
+   Mat<cx_double> W;
+   W = eye<Mat<cx_double>>(G.n1,G.n1); // Should be the size of k-space data: Is it right?
+
+   Col<cx_double> x_t;
+   x_t = pwls_pcg1<cx_double,Gfft<cx_double>,QuadPenalty<cx_double>>(xinit, G, W, TestForward, R, niter);
 	
    return x_t;
 }
