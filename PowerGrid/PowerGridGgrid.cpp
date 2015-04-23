a187fa695377a73fc872cab0cbf63313445007e1
Alex Cerjanic <acerja2@illinois.edu>
2015-04-22 23:26:03 -0500

    Replacing PowerGridMain with PowerGridGdft and PowerGridGgrid executables.

---
+++ PowerGrid/PowerGridGgrid.cpp
@@ -0,0 +1,139 @@
+//
+//  PowerGridGgrid.cpp
+//  PowerGrid
+//
+//  Created by Alex Cerjanic on 3/12/15.
+//  Copyright (c) 2015 MRFIL. All rights reserved.
+//
+
+#include "PowerGrid.h" //Project headers.
+#include "../Support/CeempleMatio.h" //Headers for using savemat and loadmat
+
+
+using namespace arma; //Armdillo stuff is in the arma namespace
+using namespace std; //complex type comes from the STL
+
+
+int main(int argc, char** argv)
+{
+    uword Nx,Ny,Nz,Niter = 1,NL = 1,Ncoils;
+
+
+    string testPath,configPath;
+    if (argc > 1) {
+        testPath = std::string(argv[1]);
+        configPath = testPath+"config.xml";
+    } else {
+      cout << "Enter a path to find test files." << endl;
+      return -1;
+    }
+
+    try
+    {
+        auto_ptr<PowerGridConfig_t> cfg(PowerGridConfig(configPath.c_str()));
+
+        Nx = cfg->Nx();
+        Ny = cfg->Ny();
+        Nz = cfg->Nz();
+        NL = cfg->Ntimeseg();
+        Niter = cfg->Niter();
+        Ncoils = cfg->Ncoils();
+
+    }
+    catch (const xml_schema::exception& e)
+    {
+        cerr << e << endl;
+        return 1;
+    }
+  
+    //string testPath = "/Users/alexcerjanic/Developer/PG/Resources/";
+
+/*
+    //Create an empty unsized matrix of type real double
+    Mat<double> test;
+
+    //Mat<cx_double> testComplex;
+    //Load our read double matrix object. (You need to match type to avoide mangling the data.)
+    loadmat(testPath+"test.mat","test",&test);
+    
+    //loadmat("/Users/alexcerjanic/Developer/PG/Resources/testForwardTest.mat","testForward",&testComplex);
+    //Create our Gfft object for type complex double and N1=64, N2=64
+    Gfft<cx_double> G(64,64);
+
+    //Perform the forward transformation and store the result in a colum vector of type complex double
+    //Note the conv_to<type to convert to>::from(data) command to convert our real double phantom to type complex double
+    Col<cx_double> testForward = G*vectorise(conv_to<Mat<cx_double>>::from(test));
+    
+    //Now perform the adjoint transform
+    Col<cx_double> testAdjoint = G/testForward;
+    
+    //Testing the my fftshift function. Armadillo delays evaluations to batch them. You can force the evaluation with data.eval()
+    //If we just passed the variable rather than the variable wrapped in a function, we wouldn't need the eval.
+    //savemat("/Users/alexcerjanic/Developer/PG/Resources/testForwardOut.mat","testForwardOut",test);
+
+    savemat("/shared/mrfil-data/acerja2/repos/PowerGrid/Resources/out.mat","fftShiftOut", fftshift(test).eval());
+    
+    //Writing the transforms to disk for evaluation in MATLAB. Note that we can't read or write complex natively (yet), so lets
+    //seperate real and imaginary first. Also, we can't put multiple variables in a single file yet. These are TODOs.
+    savemat(testPath+"testForwardReal.mat","testForwardReal", real(testForward).eval());
+    savemat(testPath+"testForwardImag.mat","testForwardImag", imag(testForward).eval());
+    savemat(testPath+"testForwardTest.mat","testForward", testForward);
+    savemat(testPath+"testAdjointTest.mat","testAdjoint", testAdjoint);
+
+    savemat(testPath+"testAdjointReal.mat","testAdjointReal", real(testAdjoint).eval());
+    savemat(testPath+"testAdjointImag.mat","testAdjointImag", imag(testAdjoint).eval());
+    
+    //Test SENSE
+
+    //Create an empty unsized matrix of type real double
+    //Mat<double> SMap;
+
+    //Load our read double matrix object. (You need to match type to avoide mangling the data.)
+    //loadmat("/Users/alexcerjanic/Developer/PG/Resources/SMap.mat","SMap",&SMap);
+
+    //create our SENSE object, coils =2
+    //SENSE<cx_double, Gfft<cx_double>> S(G,vectorise(conv_to<Col<cx_double>>::from(SMap)),4096*2,4096,2);
+
+    //Perform the forward transformation and store the result in a colum vector of type complex double
+     //Note the conv_to<type to convert to>::from(data) command to convert our real double phantom to type complex double
+     //Col<cx_double> testForward_SENSE = S*vectorise(conv_to<Mat<cx_double>>::from(test));
+
+     //Now perform the adjoint transform
+     //Col<cx_double> testAdjoint_SENSE = S/testForward_SENSE;
+
+     //savemat("/Users/alexcerjanic/Developer/PG/Resources/testForwardTest_SENSE.mat","testForward_SENSE", testForward_SENSE);
+     //savemat("/Users/alexcerjanic/Developer/PG/Resources/testAdjointTest_SENSE.mat","testAdjoint_SENSE", testAdjoint_SENSE);
+     //Test PWLS_PCG1
+    
+     //Mat<cx_double> testPWLS = test_pwls_pcg();
+     //savemat("/Users/alexcerjanic/Developer/PG/Resources/testPWLS.mat","testPWLS", testPWLS);
+    
+    Mat<cx_double> cxData;
+    Col<double> kx;
+    Col<double> ky;
+    Col<double> kz;
+    loadmat(testPath+"kx_sp.mat","kx",&kx);
+    loadmat(testPath+"ky_sp.mat","ky",&ky);
+    //loadmat("/shared/mrfil-data/jholtrop/repos/PowerGrid/Resources/data_gdft.mat","data_gdft",&cxData);
+
+    //loadmat("/Users/alexcerjanic/Developer/PG/Resources/testGdftTraj.mat","kz",&kz);
+    kz.copy_size(ky);
+    kz.zeros();
+    //Col<cx_double> out = test_ggrid<cx_double,double>(conv_to<Col<cx_double>>::from(test),kx,ky,kz);
+    //savemat(testPath+"testGdft.mat","testGdft",out);
+    */
+    //Col<cx_double> out_FieldCorrected = test_FieldCorrection<cx_double,double>();
+    //savemat(testPath+"test_FieldCorrection.mat","test_FieldCorrection",out_FieldCorrected);
+
+    /*
+    test_FieldCorrection<cx_double,double>();
+    savemat(testPath+"test_FieldCorrection.mat","test_FieldCorrection",out_FieldCorrected);
+    */
+
+    //test_FieldCorrection<cx_double,double>("/shared/mrfil-data/dataPowerGridTest/64_64_16_4coils/");
+
+    int test = test_SpeedCompareGgrid<cx_double,double>(testPath, Nx,Ny,Nz,NL,Niter,Ncoils);
+
+    return 0;
+}
+
---
+++ PowerGrid/test_SpeedCompareGgrid.hpp
@@ -0,0 +1,148 @@
+//
+//  test_SpeedCompareGgrid.h
+//  PowerGrid
+//
+//  Created by Joe holtrop on 4/7/15.
+//  Copyright (c) 2015 MRFIL. All rights reserved.
+//
+
+#ifndef __PowerGrid__SpeedCompare__Ggrid
+#define __PowerGrid__SpeedCompare__Ggrid
+
+#include "PowerGrid.h"
+
+using namespace arma;
+
+template<typename T1,typename T2>
+int test_SpeedCompareGgrid(string dataPath, uword Nx, uword Ny, uword Nz, uword L, uword niter,uword nc) {
+    string testPath = dataPath;
+
+    //uword Nx =64;
+    //uword Ny =64;
+    //uword Nz =16;
+
+    //Setup image space coordinates/trajectory
+    Cube<T2> ix(Nx,Ny,Nz);
+    Cube<T2> iy(Nx,Ny,Nz);
+    Cube<T2> iz(Nx,Ny,Nz);
+    Col<T2> FM;
+    Col<T1> SMap;
+    
+    ix.zeros();
+    iy.zeros();
+    iz.zeros();
+    
+    //generate the image space cordinates of the voxels we want to reconstruct
+    // after vectorizing ix and iy the image coordinates must match the Field and SENSe map image coordinates
+    for(uword ii = 0; ii < Ny; ii++) { //y
+        for (uword jj = 0; jj < Nx; jj++) { //x
+            for (uword kk = 0; kk < Nz; kk++) { //z
+
+                ix(ii, jj, kk) = ((T2) jj - (T2) Nx / 2.0) / ((T2) Nx);
+                iy(ii, jj, kk) = ((T2) ii - (T2) Ny / 2.0) / ((T2) Ny);
+                iz(ii, jj, kk) = ((T2) kk - (T2) Nz / 2.0) / ((T2) Nz);
+            }
+        }
+    }
+
+    Col<T2> kx;
+    loadmat(testPath+"kx.mat","kx",&kx);
+    Col<T2> ky;
+    loadmat(testPath+"ky.mat","ky",&ky);
+    Col<T2> kz;
+    loadmat(testPath+"kz.mat","kz",&kz);
+
+    uword nro;
+    nro = kx.n_elem;
+
+    Col<T2> tvec;
+    loadmat(testPath+"t.mat","t",&tvec);
+    
+    //uword L = 1;
+    loadmat(testPath+"FM.mat","FM",&FM);
+    FM.zeros();
+
+
+
+    // Fourier transfrom operator
+    cout << "Initializing Ggrid" << endl;
+    Ggrid<T1,T2> Gg(nro,2.0,Nx,Ny,Nz,kx,ky,kz,vectorise(ix),vectorise(iy),vectorise(iz));
+
+    // Field correction operation
+    cout << "Initializing FieldCorrection" << endl;
+    FieldCorrection<T1, T2, Ggrid<T1,T2>> A(Gg,vectorise(FM),vectorise(tvec),nro,Nx*Ny*Nz,L);
+
+    //cout << "Initializing Gdft" << endl;
+    //Gdft<T1,T2> Gd(nro,Nx*Ny*Nz,kx,ky,kz,vectorise(ix),vectorise(iy),vectorise(iz),vectorise(FM),vectorise(tvec));
+
+    //uword nc = 4;
+    loadmat(testPath+"SMap.mat","SMap",&SMap);
+
+    //cout << "Iniitalizing SENSE gdft" << endl;
+    //SENSE<cx_double, Gdft<T1,T2>> Sd(Gd,SMap,nro,Nx*Ny*Nz,nc);
+
+    // Sense operation
+    cout << "Iniitalizing SENSE Ggrid" << endl;
+    SENSE<cx_double, FieldCorrection<T1, T2, Ggrid<T1,T2>>> Sg(A,SMap,nro,Nx*Ny*Nz,nc);
+
+    cout << "loading data" << endl;
+    Col<T1> data;
+    loadmat(testPath+"data.mat","data",&data);
+
+    // Variables needed for the recon: Penalty object, num of iterations
+    ucube ReconMask(Nx,Ny,Nz);
+    ReconMask.ones();
+
+    cout << "Iniitalizing QuadPenalty" << endl;
+    QuadPenalty<T1>R(Nx,Ny,Nz,0,ReconMask);
+    cout << "QuadPenalty setup successfull" << endl;
+
+    //uword niter = 10;
+    Col<T1> xinit(Nx*Ny*Nz); // initial estimate of x
+    xinit.zeros();
+    T2 W;
+    //W = eye<sp_mat<T1>>(A.n1,A.n1); // Should be the size of k-space data: Is it right?
+    W=1.0;
+
+    //Col<T1> x_t;
+    //cout << "heading into PWLS_pcg1" << endl;
+    //x_t = pwls_pcg1<T1, SENSE<cx_double, FieldCorrection<T1, T2, Ggrid<T1,T2>>>,QuadPenalty<T1>>(xinit, S, W, data, R, niter);
+    //x_t = S/data;
+    //savemat(testPath+"test_3D.mat","img",x_t);
+/*
+    cout << "Runing adjoint with ggrid" << endl;
+    Col<T1> x_Sg_adjoint;
+    x_Sg_adjoint = Sg/data;
+    savemat(testPath+"test_adjoint_Sggrid.mat","img",x_Sg_adjoint);
+
+    cout << "Runing forward with ggrid" << endl;
+    Col<T1> x_Sg_forward;
+    x_Sg_forward = Sg*x_Sg_adjoint;
+    savemat(testPath+"test_forward_Sggrid.mat","img",x_Sg_forward);
+
+    cout << "Runing adjoint with gdft" << endl;
+    Col<T1> x_Sd_adjoint;
+    x_Sd_adjoint = Sd/data;
+    savemat(testPath+"test_adjoint_Sdft.mat","img",x_Sd_adjoint);
+
+    cout << "Runing forward with gdft" << endl;
+    Col<T1> x_Sd_forward;
+    x_Sd_forward = Sd*x_Sd_adjoint;
+    savemat(testPath+"test_forward_Sdft.mat","img",x_Sd_forward);
+*/
+    cout << "Runing pwls with ggrid" << endl;
+    Col<T1> test_pwls;
+    test_pwls = pwls_pcg1<T1,  SENSE<cx_double, FieldCorrection<T1, T2, Ggrid<T1,T2>>>,QuadPenalty<T1>>(xinit, Sg, W, data, R, niter);
+    savemat(testPath+"test_pwls.mat","img",test_pwls);
+/*
+    cout << "Runing pwls with ggrid" << endl;
+    Col<T1> test_pwls;
+    test_pwls = pwls_pcg1<T1,  Ggrid<T1,T2>,QuadPenalty<T1>>(xinit, Gg, W, data, R, niter);
+    savemat(testPath+"test_pwls.mat","img",test_pwls);
+    */
+
+    return 0;
+    
+}
+
+#endif
